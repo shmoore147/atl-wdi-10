953f6fb3905ea652014c6bcff75110acdf77f611
Sarah Huff Moore <sarahhuffmoore@sarahs-mbp-9.attlocal.net>
2017-05-03 21:44:49 -0400

    homework last commit

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -1,10 +1,6 @@
-// General Assembly, WDI (Web Development Immersive) Remote, Cohort 02 (R2D2)
-// Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
-// See LICENSE for details.
-
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+
   },
   drawProgressBars: function(timerValue){
     // Your Code Here
